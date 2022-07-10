3bd25ee2dc57e0113e9b5989ed2559538336411f
John Penny <jpenny1993@users.noreply.github.com>
2022-07-10 15:29:52 +0100

    Updated NiFi demo
    Many thanks to Fewnity for resolving the issue with reading from the incoming buffer

--- source/main.c
+++ source/main.c
@@ -5,15 +5,22 @@
 #include <string.h>
 
 touchPosition touchXY;
-char WIFI_Buffer[4096];
-size_t WIFI_ReceivedDataLength = 0;
-bool Wifi_ReceivedData = false;
+char WIFI_Incoming_Buffer[4096];
+size_t WIFI_Incoming_Data_Length = 0;
+bool WIFI_Data_IsReceieved = false;
+
+#define WIFI_Data (WIFI_Incoming_Buffer + 32)
+#define WIFI_Data_Size (2 * sizeof(int))
 
 void WirelessHandler(int packetID, int readlength) {
-	Wifi_RxRawReadPacket(packetID, readlength, (unsigned short *)WIFI_Buffer);
-	
-	WIFI_ReceivedDataLength = readlength - 32;
-	Wifi_ReceivedData = true;
+	// Read incoming data
+	Wifi_RxRawReadPacket(packetID, readlength, (unsigned short *)WIFI_Incoming_Buffer);
+	WIFI_Incoming_Data_Length = readlength - 32;
+
+	// Identify if wifi packet was intended for the DS
+	// Ideally you want to be checking for a message prefix, and maybe include a checksum of your message
+	// But for now we're just confirming that only x and y coordinates were sent
+	WIFI_Data_IsReceieved = WIFI_Incoming_Data_Length == WIFI_Data_Size;
 }
 
 void SendWirelessData(unsigned short *buffer, int length) {
@@ -23,7 +30,7 @@ void SendWirelessData(unsigned short *buffer, int length) {
 }
 
 void nifiInit(void) {
-	iprintf("\n\n\tNiFi initiating\n\n");
+	iprintf("\nNiFi initiating\n\n");
 	
 	// Changes how packets are handled
 	// this seems to modify the frame much like Wifi_TransmitFunction()
@@ -48,36 +55,34 @@ void nifiInit(void) {
 }
 
 void gameLoop(void) {
-	// Buffer must be aligned to 2 bytes, so use unsigned short
-	unsigned short buffer[(2 * sizeof(int)) / sizeof(unsigned short)];
-	
-	// Get stylus location
-	touchRead(&touchXY);
+	int heldDown = keysHeld();
+	bool isStylusHeld = heldDown & KEY_TOUCH;
 
-	// print at using ansi escape sequence \x1b[line;columnH 
-	iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
-	iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);
+	if (isStylusHeld) {
+		// Get stylus location
+		touchRead(&touchXY);
+		
+		// print at using ansi escape sequence \x1b[line;columnH 
+		iprintf("\x1b[16;0HTouch x = %04X\n", touchXY.px);
+		iprintf("Touch y = %04X\n", touchXY.py);
 
-	// Send Outgoing Packets
-	// if (DSGM_held.Stylus) {
-		buffer[0] = touchXY.px;
-		buffer[1] = touchXY.py;
-		SendWirelessData(buffer, 2 * sizeof(int));
-	// }
-	
-	// Handle Incoming Packets
-	if (Wifi_ReceivedData) {
-		// This will happen from pretty much anything, my house is pretty noisey
-		iprintf("\n\n\tNiFi received\n\n");
-		if (WIFI_ReceivedDataLength == 2 * sizeof(int)) {
-			// TODO: get data to be received consitently and printed out visible
-			unsigned short x = (unsigned short)WIFI_Buffer[0 * sizeof(unsigned short)];
-			unsigned short y = (unsigned short)WIFI_Buffer[1 * sizeof(unsigned short)];
-			iprintf("\x1b[7;0HNiFi x = %04X, y = %04X\n", x, y);
-		}
+		// Buffer must be aligned to 2 bytes, so use unsigned short
+		unsigned short outgoing_buffer[WIFI_Data_Size / sizeof(unsigned short)];
+		outgoing_buffer[0] = touchXY.px;
+		outgoing_buffer[1] = touchXY.py;
+
+		// Send outgoing packet
+		SendWirelessData(outgoing_buffer, WIFI_Data_Size);
+	}
+
+	if (WIFI_Data_IsReceieved) {
+		// Process your data packet here
+		unsigned short x = (unsigned short)WIFI_Data[0 * sizeof(unsigned short)];
+		unsigned short y = (unsigned short)WIFI_Data[1 * sizeof(unsigned short)];
+		iprintf("\x1b[7;0HNiFi x = %04X, y = %04X\n", x, y);
 
 		// Reset once data received
-		Wifi_ReceivedData = false;
+		WIFI_Data_IsReceieved = false;
 	}
 }
 
@@ -89,8 +94,12 @@ int main(void) {
 
 	while(1) {
 		swiWaitForVBlank();
-			int keys = keysDown();
-			if(keys & KEY_START) break;
+		
+		// Read the controller input state
+		scanKeys();
+
+		int keys = keysDown();
+		if(keys & KEY_START) break;
 
 		gameLoop();
 	}
